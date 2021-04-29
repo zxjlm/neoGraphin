f5c0bf86064b990f337c0ab49bfcd1c40753da10
harumonia <zxjlm233@gmail.com>
2021-04-29 18:55:38 +0800

    chore: add tree selector layout

--- src/components/TreeSelector.tsx
+++ src/components/TreeSelector.tsx
@@ -6,8 +6,6 @@ import {DataNode} from "rc-tree-select/lib/interface";
 const {SHOW_PARENT} = TreeSelect;
 
 export const TreeSelector = ({options}: { options: DataNode[] }) => {
-    console.log('tree selector', options)
-
     const onChange = () => {
 
     }
@@ -20,6 +18,7 @@ export const TreeSelector = ({options}: { options: DataNode[] }) => {
             treeCheckable={true}
             showCheckedStrategy={SHOW_PARENT}
             placeholder={'Please select'}
+            allowClear={true}
             style={
                 {
                     width: '100%',
